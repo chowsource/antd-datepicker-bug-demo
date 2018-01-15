09742d40c57b15552d5e834ad0983517a0f95686
kris.zhou <kris.zhou@coupang.com>
2018-01-15 17:41:28 +0800

    RangePicker demo for 3.1.3

--- src/DemoComponent.js
+++ src/DemoComponent.js
@@ -1,19 +1,20 @@
 import React from 'react';
 
 import { LocaleProvider ,Button, DatePicker,version} from 'antd';
-// import enUS from 'antd/es/locale-provider/en_US';
+import enUS from 'antd/es/locale-provider/en_US';
 import zhCN from 'antd/es/locale-provider/zh_CN';
 
+const { RangePicker } = DatePicker;
 
 export default class DemoComponent extends React.Component {
 
-    state = {local:null};
+    state = {local:enUS};
 
 
     changeLocale = () => {
         const { local } = this.state;
-        if(local){
-            this.setState({local:null});
+        if(local === zhCN){
+            this.setState({local:enUS});
         }else{
             this.setState({local:zhCN});
         }
@@ -27,9 +28,10 @@ export default class DemoComponent extends React.Component {
             <LocaleProvider locale={local}>
                 <div>
                     <p>Current Version:{version}</p>
-                    <Button type='primary' onClick={this.changeLocale}>{local ? '当前中文' : '当前English'}</Button>
+                    <Button type='primary' onClick={this.changeLocale}>{local === zhCN ? '当前中文' : '当前English'}</Button>
                     <br/>
                     <DatePicker/>
+                    <RangePicker />
                 </div>
             </LocaleProvider>
         );
